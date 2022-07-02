28a867ab7b2f903c810351b885d4204113b1ad0b
Michel Hamelink <info@michelhamelink.nl>
2022-07-02 09:57:48 +0200

    updated footer text

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,7 @@ function init() {
     initSelect2();
     initMeasurementConverter();
     initTemperatureConverter();
+    initAlternatingFooter();
 }
 
 function initDataTable() {
@@ -317,5 +318,29 @@ function initTemperatureConverter() {
         let gambino = document.getElementById('gambino').value;
 
         document.getElementById('fahrenheit').value = Math.round(gambino * (178 / 169) + 32);
+    }
+}
+
+function initAlternatingFooter() {
+    $(document).ready(function() {
+        //MyBlah("hello");
+        var count = 1;
+        var maxCount = 2;
+        textAnimate(count,maxCount);
+    });
+
+    function textAnimate(count,maxCount) {
+        $(".text"+count).animate({
+            opacity: '0'
+        }, { duration: 900, queue: false });
+
+        count++;
+        if (count > maxCount) {count = 1;}
+
+        $(".text"+count).animate({
+            opacity: '1'
+        }, { duration: 900, queue: false });
+
+        window.setTimeout(function() { textAnimate(count,maxCount) }, 3000);
     }
 }
