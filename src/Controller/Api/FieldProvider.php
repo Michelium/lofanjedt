bfc21049e69d93f2d6af94adfc1c3f20469b8755
Michel Hamelink <info@michelhamelink.nl>
2023-08-28 10:06:26 +0200

    updated get form api endpoint layout

--- src/Controller/Api/FieldProvider.php
+++ src/Controller/Api/FieldProvider.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Api;
 
 use App\Entity\Entry;
+use App\Form\EntryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\ChoiceList\Loader\CallbackChoiceLoader;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,15 +15,44 @@ use Symfony\Component\Routing\Annotation\Route;
 class FieldProvider extends AbstractController {
 
     #[Route('/fields', name: 'api-fields-get', methods: 'GET')]
-    public function getCategories(Request $request): JsonResponse {
+    public function getCategories(Request $request, FormFactoryInterface $formFactory): JsonResponse {
         $category = $request->get('category');
         $fields = $category === null ? Entry::FIELDS : Entry::FIELDS[$category];
-        
-        if ($request->get('human_readable') == 1) {
-            $fields = $category === null ? Entry::HUMAN_FIELDS : Entry::HUMAN_FIELDS[$category];
+
+        $entry = new Entry();
+        $form = $formFactory->create(EntryType::class, $entry);
+
+        $fieldDetails = [];
+
+        foreach ($fields as $fieldName) {
+            $fieldInfo = [
+                'name' => $fieldName,
+            ];
+
+            // Get the form field configuration using the form instance
+            $formField = $form->get($fieldName);
+            $fieldConfig = $formField->getConfig();
+            
+            $fieldInfo['type'] = $fieldConfig->getType()->getBlockPrefix();
+            $fieldInfo['required'] = $fieldConfig->getOption('required') ?? false;
+            $fieldInfo['label'] = $fieldConfig->getOption('label') ?? false;
+
+            // Check if choices are set directly
+            $choices = $fieldConfig->getOption('choices');
+            if (!empty($choices)) {
+                $fieldInfo['choices'] = $choices;
+            }
+
+            // Check if choices are set using a choice_loader callback
+            $choiceLoader = $fieldConfig->getOption('choice_loader');
+            if ($choiceLoader instanceof CallbackChoiceLoader) {
+                $fieldInfo['choices'] = $choiceLoader->loadChoiceList()->getChoices();
+            }
+            
+            $fieldDetails[] = $fieldInfo;
         }
-        
-        return new JsonResponse($fields);
+
+        return new JsonResponse($fieldDetails);
     }
 
 }
